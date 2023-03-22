5cf7965af41e434562672223a2043f97fa1b97c4
Souvi <souvima@hotmail.com>
2023-03-22 23:18:49 +0100

    Fix bug hunt (images/names files) Bills

--- FrontEnd/src/__tests__/NewBill.js
+++ FrontEnd/src/__tests__/NewBill.js
@@ -12,6 +12,21 @@ describe("Given I am connected as an employee", () => {
       const html = NewBillUI();
       document.body.innerHTML = html;
       //to-do write assertion
+
+      const fileType = file.type;
+      const $fileError = this.document.getElementById("file-error");
+      const $submitBtn = this.document.getElementById("btn-send-bill");
+      if (fileType !== "image/jpeg" && fileType !== "image/png") {
+        $fileError.style.display = "block";
+        $fileError.style.color = "red";
+        $fileError.innerHTML =
+          "Veuillez choisir une image ou fichier au format .jpg, .jpeg ou .png";
+        $submitBtn.disabled = true;
+        return;
+      } else {
+        $fileError.style.display = "none";
+        $submitBtn.disabled = false;
+      }
     });
   });
 });
